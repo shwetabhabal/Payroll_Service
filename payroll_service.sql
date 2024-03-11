aaad68aea2dcca2026c2d015d756a3ff313231f9
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-11 11:49:37 +0530

    insert data

--- payroll_service.sql
+++ payroll_service.sql
@@ -5,5 +5,10 @@ create table employee_payroll (
 id int primary key auto_increment,
 name varchar(30),
 salary int,
-start_date int
+start_date date
 );
+insert into employee_payroll values
+(1,"ravi",5000,20-1-2024);
+insert into employee_payroll values
+(2,"om",6000,22-1-2024),
+(3,"prakash",7000,24-1-2024);
