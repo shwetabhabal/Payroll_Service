78136ac1e1bbed8321dc4840d27fa67c94d6343d
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-11 11:41:50 +0530

    create table

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,3 +1,9 @@
 create database payroll_service;
 show databases;
 use payroll_service;
+create table employee_payroll (
+id int primary key auto_increment,
+name varchar(30),
+salary int,
+start_date int
+);
