26508b9d9740b923d6897e8d7c7bfe528efb23cd
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-03-11 12:36:00 +0530

    retrive particular data

--- payroll_service.sql
+++ payroll_service.sql
@@ -7,9 +7,14 @@ name varchar(30),
 salary int,
 start_date date
 );
+insert into employee_payroll values(1,"ravi",5000,"2024-01-01");
 insert into employee_payroll values
-(1,"ravi",5000,20-1-2024);
-insert into employee_payroll values
-(2,"om",6000,22-1-2024),
-(3,"prakash",7000,24-1-2024);
-select * from employee_payroll;
+(2,"om", 6000,"2024-01-15"),
+(3,"jay", 7000,"2024-02-01"),
+(4,"prakash",5000,"2023-12-01"),
+(5,"diva", 5000,"2023-11-01"),
+(6,"raj", 6000, "2023-10-01");
+select* from employee_payroll;
+select salary from employee_payroll where name = "jay";
+select salary from employee_payroll where
+start_date between cast("2024-01-01" as date) and date(now());
